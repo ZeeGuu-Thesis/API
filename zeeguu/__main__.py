2515d9bb80e845a3755af095ef590076c5eba48f
Simon Marti <simon@ceilingcat.ch>
2013-09-02 12:17:37 +0000

    Added options for host and port in dev mode

--- zeeguu/__main__.py
+++ zeeguu/__main__.py
@@ -3,4 +3,8 @@
 import zeeguu
 
 
-zeeguu.app.run()
+print "Instance folder:", zeeguu.app.instance_path
+zeeguu.app.run(
+    host=zeeguu.app.config.get("HOST", "localhost"),
+    port=zeeguu.app.config.get("PORT", 9000)
+)            
